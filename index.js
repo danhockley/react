bf46566c5fee847fc8b33ceb9fd3fda782441ac2
Daniel Hockley <danhockley1@gmail.com>
2021-02-22 16:42:42 +0200

    Seasons Application - Simple

---
+++ index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import "semantic-ui-css/semantic.min.css";
+import Spinner from './Spinner';
+
+class App extends React.Component {
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <SeasonDisplay lat = { this.state.lat }
+            />
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat = { this.state.lat }
+            />
+        } 
+        return <Spinner message='Waiting on location access...'/>;
+    }
+
+    render() {
+        return (
+            <div className='border red'>
+                {this.renderContent()}
+            </div>
+        );
+}
+}
+
+ReactDOM.render( <
+    App / > ,
+    document.querySelector("#root")
+);
